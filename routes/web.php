bd0cee7e6ad1a95fe60cbdf095acb6412ebc2a29
alessandrorumi <rumi.alessandro21@gmail.com>
2024-02-15 13:51:01 +0100

    Scafflding & Add: Model, Migration

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+use App\Http\Controllers\ProductController;
 
-    return view('pages.index');
-});
+Route :: get('/', [EmployeeController :: class, 'index'])->name('users.index');
